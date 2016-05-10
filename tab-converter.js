8500c7b12e6b14ae1efd0aaf65f218f2b7158d5d
Bret <blackbret94@gmail.com>
2016-05-10 02:36:56 -0400

    Split into multiple files.  Now correctly converts guitar tab into mandolin tab

---
+++ tab-converter.js
@@ -0,0 +1,109 @@
+// guitar strings
+var inStrings = [noteToNumber("E",4),noteToNumber("B",3),noteToNumber("G",3),noteToNumber("D",3),noteToNumber("A",2),noteToNumber("E",2)];
+
+// mandolin strings
+var outStrings = [noteToNumber("E",5),noteToNumber("A",4),noteToNumber("D",4),noteToNumber("G",3)];
+
+
+function genTab() {
+    converted = transpose( $('#in-tab').val());
+    $('#out-tab').val(converted);
+    return false;
+}
+
+// transpose
+function transpose(tab){
+    // split on lines
+    var lineTab = tab.split("\n"); // split by line
+    var splitTab = []; // split by -
+    var outTab = new Array(outStrings); // what is printed
+
+    // init
+    for(var i=0;i<outStrings.length;i++){
+        outTab[i] = [];
+        for(var j=0;j<lineTab[i].length;j++){
+            outTab[i][j]="-";
+        }
+    }
+
+    // fill
+    for(var i=0;i<lineTab.length && i<inStrings.length;i++){
+        // split frets
+        splitTab[i] = lineTab[i];//.split("-");
+
+        // adjust
+        for(var j=0;j<splitTab[i].length;j++){
+            // attempt to parse as int
+            var parsed = parseInt(splitTab[i][j]);
+
+            if(!isNaN(parsed) && parsed != null) {
+                // convert to noteNumber
+                var noteNumber = parsed + inStrings[i];
+                var bestString = pickString(noteNumber);
+                console.log(noteNumber + " " + bestString);
+                outTab[bestString][j] = toFret(noteNumber,outStrings[bestString]) + "-";
+            // 
+            } else if(i<outStrings.length){
+                /*if(splitTab[i][j] != ""){
+                    outTab[i][j] = splitTab[i][j];
+                } else {
+                    outTab[i][j] = "-";
+                }*/
+            }
+        }
+    }
+
+    // reform
+    var reformedTab = ""; // string sent to output
+
+    for(var i=0;i<outStrings.length;i++){
+        for(var j=0;j<outTab[i].length;j++){
+            reformedTab = reformedTab.concat(outTab[i][j]);
+        }
+        reformedTab = reformedTab.concat("\n")
+    }
+
+    return reformedTab;
+}
+
+// takes a note and octive and returns an int
+function noteToNumber(note, octive){
+    var noteNum = 0;
+    switch(note){
+        case "C": noteNum = 0; break;
+        case "C#": noteNum = 1; break;
+        case "D": noteNum = 2; break;
+        case "Eb": noteNum = 3; break;
+        case "E": noteNum = 4; break;
+        case "F": noteNum = 5; break;
+        case "F#": noteNum = 6; break;
+        case "G": noteNum = 7; break;
+        case "Ab": noteNum = 8; break;
+        case "A": noteNum = 9; break;
+        case "Bb": noteNum = 10; break;
+        case "B": noteNum = 11; break;
+    }
+
+    return noteNum + (octive * 12);
+}
+
+// finds the most appropriate string for a note, restricts to limited frets
+function pickString(noteNum){
+    var bestString = 0; // what string is most appropriate?
+    var bestDistance = 100000; // how far am I from the open string?
+
+    for(var k=0; k<outStrings.length; k++){
+        var thisDistance = noteNum-outStrings[k];
+        if(thisDistance < bestDistance && thisDistance >= 0){
+            bestString = k;
+            bestDistance = thisDistance;
+        }
+    }
+
+    return bestString;
+}
+
+// converts from a raw note number to a fret number
+function toFret(noteNum, stringNoteNumber){
+    return noteNum-stringNoteNumber;
+}
